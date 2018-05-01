c1fcc80a022a9326201c479f2539696e16716c44
anass <ans-ezzouaine@hotmail.com>
2018-05-01 13:43:20 +0100

    publish all config

--- config/app.php
+++ config/app.php
@@ -159,6 +159,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Reliese\Coders\CodersServiceProvider::class,
+        App\Providers\RepositoryServiceProvider::class
+
 
     ],
 
